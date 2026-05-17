e74944fa0c65ed911ed90137509f79ddc4836321
Udita Chandel <uditachandel8@gmail.com>
2026-05-17 11:13:23 +0530

    Fixed signup UI

--- app/(routes)/dashboard/page.jsx
+++ app/(routes)/dashboard/page.jsx
@@ -8,76 +8,76 @@ import { getTableColumns, sql, eq, desc } from "drizzle-orm";
 import BarChartDashboard from "./_components/BarChartDashboard";
 import BudgetItem from "./budgets/_components/BudgetItem";
 import ExpenseListTable from "./expenses/_components/ExpenseListTable";
-function Dashboard(){
-    const{user}=useUser();
+function Dashboard() {
+  const { user } = useUser();
 
-    const[budgetList,setBudgetList]=useState([]);
-    const[expensesList,setExpensesList]=useState([]);
-  useEffect(()=>{
-    user&&getBudgetList();
-  },[user])
-  
+  const [budgetList, setBudgetList] = useState([]);
+  const [expensesList, setExpensesList] = useState([]);
+  useEffect(() => {
+    user && getBudgetList();
+  }, [user])
+
   /* Used To Get Budget List*/
-  const getBudgetList=async()=>{
+  const getBudgetList = async () => {
     if (!user?.primaryEmailAddress?.emailAddress) return;
 
-    const result=await db.select({
-      
+    const result = await db.select({
 
-    ...getTableColumns(Budgets),
-     
-      totalSpend:sql `sum(${Expenses.amount})`.mapWith(Number),
-      totalItem:sql `count(${Expenses.id})`.mapWith(Number)
+
+      ...getTableColumns(Budgets),
+
+      totalSpend: sql`sum(${Expenses.amount})`.mapWith(Number),
+      totalItem: sql`count(${Expenses.id})`.mapWith(Number)
     }).from(Budgets)
-    .leftJoin(Expenses,eq(Budgets.id,Expenses.budgetId))
-    .where(eq(Budgets.createdBy,user?.primaryEmailAddress?.emailAddress))
-    .groupBy(Budgets.id)
-    .orderBy(desc(Budgets.id))
-    ;
-    
+      .leftJoin(Expenses, eq(Budgets.id, Expenses.budgetId))
+      .where(eq(Budgets.createdBy, user?.primaryEmailAddress?.emailAddress))
+      .groupBy(Budgets.id)
+      .orderBy(desc(Budgets.id))
+      ;
+
     setBudgetList(result);
     getAllExpenses();
-   
+
   }
   /* Used To Get All Expenses Belonging To User */
-  const getAllExpenses=async()=>{
-    const result=await db.select({
-        id:Expenses.id,
-        name:Expenses.name,
-        amount: Expenses.amount,
-        createdAt:Expenses.createdAt
+  const getAllExpenses = async () => {
+    const result = await db.select({
+      id: Expenses.id,
+      name: Expenses.name,
+      amount: Expenses.amount,
+      createdAt: Expenses.createdAt
     }).from(Budgets)
-    .rightJoin(Expenses,eq(Budgets.id,Expenses.budgetId))
-    .where(eq(Budgets.createdBy,user?.primaryEmailAddress.emailAddress))
-    .orderBy(desc(Expenses.id));
+      .rightJoin(Expenses, eq(Budgets.id, Expenses.budgetId))
+      .where(eq(Budgets.createdBy, user?.primaryEmailAddress.emailAddress))
+      .orderBy(desc(Expenses.id));
     setExpensesList(result);
-    
-}
 
-    return(
+  }
+
+  return (
     <div className="p-8">
-     <h2 className="font-bold text-3xl"> Hi, {user?.fullName} ✌🏼</h2>
-     <p className="text-gray-500 "> “Here’s how your money is being spent—let’s take control of your expenses!”</p>
-    <CardInfo budgetList={budgetList}/>
-    <div className=" grid grid-cols-1 md:grid-cols-3 mt-6 gap-5">
-        <div className="md:col-span-2"> 
-           <BarChartDashboard
-           budgetList={budgetList}
-           />
+      <h2 className="font-bold text-3xl"> Hi, {user?.fullName} ✌🏼</h2>
+      <p className="text-gray-500 "> “Here’s how your money is being spent—let’s take control of your expenses!”</p>
+      <CardInfo budgetList={budgetList} />
+      <div className=" grid grid-cols-1 md:grid-cols-3 mt-6 gap-5">
+        <div className="md:col-span-2">
+          <BarChartDashboard
+            budgetList={budgetList}
+          />
 
-           <ExpenseListTable
-           expensesList={expensesList}
-           refreshData={()=>getBudgetList()}
-           />
+          <ExpenseListTable
+            expensesList={expensesList}
+            refreshData={() => getBudgetList()}
+          />
         </div>
         <div className="grid gap-2">
-            <h2 className="font-bold text-lg">Latest Budgets</h2>
-            {budgetList.map((budget,index)=>(
-                <BudgetItem budget={budget} key={index} />
-            ))}
+          <h2 className="font-bold text-lg">Latest Budgets</h2>
+          {budgetList.map((budget, index) => (
+            <BudgetItem budget={budget} key={index} />
+          ))}
         </div>
       </div>
     </div>
-)
+  )
 }
 export default Dashboard
